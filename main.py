da38617ffcfb4ed049e42ecf8ece817b5ea2d336
Jtolen543 <Jtolen543@gmail.com>
2024-10-01 09:37:50 -0400

    Finished text to speech translator

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from openai import OpenAI
+from openai import OpenAI, RateLimitError
 import pdfplumber
 from tkinter.filedialog import *
 from tkinter import *
@@ -16,22 +16,29 @@ file_path = askopenfile(title="Select PDF file", filetypes=[("PDF files", ".pdf"
 text = ""
 
 pdf = pdfplumber.open(file_path)
+try:
+    for page in pdf.pages:
+        text += page.extract_text()
 
-for page in pdf.pages:
-    text += page.extract_text()
+    client = OpenAI(api_key=key)
 
-client = OpenAI(api_key=key)
+    with client.audio.speech.with_streaming_response.create(
+        model="tts-1",
+        voice="alloy",
+        input=text,
+    ) as response:
+        response.stream_to_file("output.mp3")
 
-with client.audio.speech.with_streaming_response.create(
-    model="tts-1",
-    voice="alloy",
-    input=text,
-) as response:
-    response.stream_to_file("speech.mp3")
-
-file_name = asksaveasfilename(defaultextension=".mp3", filetypes=[("MP3 File", '.mp3')])
-with open("output.mp3", "rb") as src:
-    data = src.read()
-with open(file_name, "wb") as file:
-    file.write(data)
-
+    file_name = asksaveasfilename(defaultextension=".mp3", filetypes=[("MP3 File", '.mp3')])
+    with open("output.mp3", "rb") as src:
+        data = src.read()
+    with open(file_name, "wb") as file:
+        pass
+        file.write(data)
+    print("You PDF file has been successfully converted into a text-to-speech prompt")
+except AttributeError:
+    print("Please select a file to use the TTS converter")
+except RateLimitError:
+    print("Please add credits to your account to use the converter")
+except FileNotFoundError:
+    print("Please enter a name to save the MP3 file")
